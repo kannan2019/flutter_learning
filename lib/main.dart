15b8cfc79b6e29c58d7699e8594b8a8cf4f6ca2a
Kannan S <kannan-inbox@outlook.com>
2020-02-03 22:13:40 +0530

    Added Sample Code

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+//* void main is the starting point of the flutter app
+void main() => runApp(
+      MaterialApp(
+        home: Center(
+          child: Text('Kannan'),
+        ),
+      ),
+    );
