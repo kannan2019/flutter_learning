10f10a9523070dae50e82e8e61e00c8beef24d7c
Kannan S <kannan-inbox@outlook.com>
2020-02-03 23:15:35 +0530

    Added Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,30 @@ import 'package:flutter/material.dart';
 //* void main is the starting point of the flutter app
 void main() => runApp(
       MaterialApp(
-        home: Center(
-          child: Text('Kannan'),
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey[200],
+          appBar: AppBar(
+            title: Text('My First App'),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Image(
+            image: NetworkImage(
+                'https://images.unsplash.com/photo-1534543210152-32025bcfaad9?ixlib=rb-1.2.1&w=1000&q=80'),
+          ),
+          bottomNavigationBar: BottomAppBar(
+            shape: CircularNotchedRectangle(),
+            child: Container(
+              height: 50.0,
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () => {},
+            tooltip: "Add new Item",
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
         ),
       ),
     );
